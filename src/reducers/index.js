29dc794e2e7a69c8191fb8bec512882b211e4630
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-10-30 06:23:40 +1100

    reducers index.js dome

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+//2 reducers - list of song, selected song
+//return array of object of songs(static now)
+//selected songreducers takes two arguments: 1. at the begining there will be no song selected, so the selectedsoong will be null, second argumnt action object have type and payload properties
+//it scans : if the action type is === to SONG_SELECTED type from the index.js file of actions folder it will return the slected song. otherwise it will return the selected song which is null at the begining
+
+const songsReducer = () => {
+  return [
+    { title: "song 1", durations: "4:00" },
+    { title: "song 2", durations: "3:00" },
+    { title: "song 3", durations: "5:00" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
